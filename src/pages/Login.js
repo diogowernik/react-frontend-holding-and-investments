11e9c199dc505c9831de0cd325bf96c004149483
Nick Maslov <kategorychnost@gmail.com>
2021-10-19 12:09:19 -0400

    added navbar hoc layout with bootstrap

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import MainLayout from '../layouts/MainLayout';
 
 const Login = () => {
-  return <div>Login</div>;
+  return <MainLayout>Login</MainLayout>;
 };
 
 export default Login;
