e9301ca5d0fee64fefc4ecf432d55e7f04331ec8
Diogo Wernik <11983260+diogowernik@users.noreply.github.com>
2024-01-12 20:21:48 -0300

    build

--- src/pages/EvolutionTemplate.js
+++ src/pages/EvolutionTemplate.js
@@ -1,14 +1,15 @@
-import { Row, Col, Container, Card} from 'react-bootstrap';
+import React, { useEffect, useState, useContext, useCallback } from 'react';
+import { Row, Col, Container, Card } from 'react-bootstrap';
 import MainLayout from '../layouts/MainLayout';
 import { fetchPortfolioEvolution } from '../apis';
 import AuthContext from '../contexts/AuthContext';
-import React, { useEffect, useState, useContext, useCallback } from 'react';
-import { useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { filterGroupMap } from '../group_functions';
 import PortfolioNavTemplate from '../components/nav/PortfolioNavTemplate';
 
 const EvolutionTemplate = ({ currency }) => {
     const [portfolio_evolution, setPortfolioEvolution] = useState([]);
+    const [activeYear, setActiveYear] = useState('');
 
     const auth = useContext(AuthContext);
     const params = useParams();
@@ -18,100 +19,113 @@ const EvolutionTemplate = ({ currency }) => {
         if (json) {
             setPortfolioEvolution(json);
         }
-        }, [params.id, auth.token]);
-        useEffect(() => {
+    }, [params.id, auth.token]);
+
+    useEffect(() => {
         onFetchPortfolioEvolution();
-        }, [onFetchPortfolioEvolution]);
+    }, [onFetchPortfolioEvolution]);
 
-    
-    
-    const category_evolution = filterGroupMap(portfolio_evolution, "date")
-    // order categories data by category
-    category_evolution.forEach((category) => {
-        category.data.sort((a, b) => {
-            if (a.category > b.category) {
-                return 1;
-            }
-            if (a.category < b.category) {
-                return -1;
-            }
-            return 0;
-        })
-    })
-    const sumCategoryData = currency => category_evolution.map((name, data) => {
-        const category_total = name.data.reduce((acc, cur) => {
-          return acc + cur[`category_total_${currency}`];
-        }, 0);
-        return {
-          name: name.name,
-          total: category_total
-        }
-      });
-      
-    const category_evolution_sum = sumCategoryData(currency);
+    useEffect(() => {
+        const years = portfolio_evolution.map(item => item.date.split('/')[2]);
+        const uniqueYears = [...new Set(years)];
+        setActiveYear(uniqueYears.sort().reverse()[0]);
+    }, [portfolio_evolution]);
 
-    const combinedData = category_evolution.reduce((acc, cur) => {
-        return [...acc, ...cur.data];
-      }, []);
-      
-    const categoryMap = combinedData.reduce((acc, cur) => {
-    if(!acc[cur.category]) {
-        acc[cur.category] = [cur];
-        return acc;
-    }
-    acc[cur.category].push(cur);
-    return acc;
-    }, {});
+    const renderTabs = () => {
+        const years = portfolio_evolution.map(item => item.date.split('/')[2]);
+        const uniqueYears = [...new Set(years)];
 
+        return (
+            <ul className="nav nav-tabs">
+                {uniqueYears.map(year => (
+                    <li className="nav-item" key={year}>
+                        <a 
+                            className={`nav-link ${activeYear === year ? 'active' : ''}`}
+                            onClick={() => setActiveYear(year)}
+                        >
+                            {year}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        );
+    };
 
-  return (
-    <MainLayout>
-    <PortfolioNavTemplate currency={currency} />
-      <Container fluid>
-        <Row>
-            <Col md={12}>
-                <Card>
-                    <Card.Header>
-                        <Card.Title>Crescimento Patrimonial</Card.Title>
-                    </Card.Header>
-                    <Card.Body>
-                        <table className="table table-sm table-hover table-striped table-bordered table-responsive-sm">
-                            <thead>
-                                <tr>
-                                <th scope="col">Categoria</th>
-                                {category_evolution.map(({ name }) => (
-                                    <th scope="col" key={name}>{name}</th>
-                                ))}
-                                </tr>
-                            </thead>
-                            <tbody>
-                            {Object.entries(categoryMap).map(([category, values]) => (
-                                <tr key={category}>
-                                    <th>{category}</th>
-                                    {values.map(({ id, ...category_total }) => (
-    <td key={id}>{category_total[`category_total_${currency}`].toLocaleString('pt-BR', { style: 'currency', currency: currency.toUpperCase() })}</td>
-))}
-                                </tr>
+    const renderTabContent = () => {
+        const filteredEvolution = portfolio_evolution.filter(item => item.date.includes(activeYear));
+        const filteredCategoryEvolution = filterGroupMap(filteredEvolution, "date");
+
+        filteredCategoryEvolution.forEach(category => {
+            category.data.sort((a, b) => a.category > b.category ? 1 : -1);
+        });
+
+        const category_evolution_sum = filteredCategoryEvolution.map(name => {
+            const category_total = name.data.reduce((acc, cur) => acc + cur[`category_total_${currency}`], 0);
+            return { name: name.name, total: category_total };
+        });
+
+        const combinedData = filteredCategoryEvolution.reduce((acc, cur) => [...acc, ...cur.data], []);
+        const categoryMap = combinedData.reduce((acc, cur) => {
+            acc[cur.category] = acc[cur.category] || [];
+            acc[cur.category].push(cur);
+            return acc;
+        }, {});
+
+        return (
+            <table className="table table-sm table-hover table-striped table-bordered table-responsive-sm">
+                <thead>
+                    <tr>
+                        <th scope="col">Categoria</th>
+                        {filteredCategoryEvolution.map(({ name }) => (
+                            <th scope="col" key={name}>{name}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {Object.entries(categoryMap).map(([category, values]) => (
+                        <tr key={category}>
+                            <th>{category}</th>
+                            {values.map(({ id, ...category_total }) => (
+                                <td key={id}>
+                                    {category_total[`category_total_${currency}`].toLocaleString('pt-BR', { style: 'currency', currency: currency.toUpperCase() })}
+                                </td>
                             ))}
-                            <tr key="total" className="text-primary font-weight-bold">
-                                <th>Total</th>
-                          {category_evolution_sum.map(({ total }) => (
-                                <td key={total}>{total.toLocaleString('pt-BR', { style: 'currency', currency: currency.toUpperCase() })}</td>
-                            ))}
-                            </tr>
-                            </tbody>
-                        </table>
-                    </Card.Body>
-                    
-                
-                </Card>
-            </Col>
-        </Row>
-        
-      </Container>
-      
-    </MainLayout>
-  )
+                        </tr>
+                    ))}
+                    <tr key="total" className="text-primary font-weight-bold">
+                        <th>Total</th>
+                        {category_evolution_sum.map(({ total }) => (
+                            <td key={total}>
+                                {total.toLocaleString('pt-BR', { style: 'currency', currency: currency.toUpperCase() })}
+                            </td>
+                        ))}
+                    </tr>
+                </tbody>
+            </table>
+        );
+    };
+
+    return (
+        <MainLayout>
+            <PortfolioNavTemplate currency={currency} />
+            <Container fluid>
+                <Row>
+                    <Col md={12}>
+                        <Card>
+                            <Card.Header>
+                                <Card.Title>Crescimento Patrimonial</Card.Title>
+                            </Card.Header>
+                            <Card.Body>
+                                {renderTabs()}
+                                {renderTabContent()}
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </MainLayout>
+    );
 };
 
 export default EvolutionTemplate;
+
